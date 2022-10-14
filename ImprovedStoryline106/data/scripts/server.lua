faaf59c9442817aac227dd5b2deeaccda4b09c02
zatinu322 <pavl05@mail.ru>
2022-10-14 16:18:04 +0300

    Fixed inventory size, some 7 dev problems, version number

--- ImprovedStoryline106/data/scripts/server.lua
+++ ImprovedStoryline106/data/scripts/server.lua
@@ -12,9 +12,9 @@
 -- ----------------------------------------------------------------------------
 
 if ISL_VERSION == nil then
-	LOG("IMPROVED STORYLINE V1.0.6 HD BUILD 220927")
+	LOG("IMPROVED STORYLINE V1.0.6 HD BUILD 221014")
 
-	ISL_VERSION = "106-HD-220927"
+	ISL_VERSION = "106-HD-221014"
 
 	-- only for development needs, should not be in release!
 	EXECUTE_SCRIPT "data\\scripts\\compatch.lua"

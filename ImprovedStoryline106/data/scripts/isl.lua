d7a9e4fe5566f94ecb1ed0379bea16097ba03dd8
zatinu322 <pavl05@mail.ru>
2022-11-04 00:01:37 +0300

    Improvements for new 221104 build
    Flags and some other beautiful corrections

--- ImprovedStoryline106/data/scripts/isl.lua
+++ ImprovedStoryline106/data/scripts/isl.lua
@@ -1,7 +1,7 @@
 if ISL_VERSION == nil then
-	LOG("IMPROVED STORYLINE V1.0.6 BUILD 221014")
+	LOG("IMPROVED STORYLINE V1.0.6 BUILD 221104")
 
-	ISL_VERSION = "106-221014"
+	ISL_VERSION = "106-221104"
 
 	-- only for development needs, should not be in release!
 	EXECUTE_SCRIPT "data\\scripts\\compatch.lua"

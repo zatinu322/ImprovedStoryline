4122a6a4d41cc0b22bc74422b2d547b21c3527a2
zatinu322 <pavl05@mail.ru>
2022-11-12 17:10:14 +0300

    New for build-221112
    Deleted a lot of unused sounds, fixed Monastery gates, new npc backgrounds, fixes from 7, new build-ready

--- ImprovedStoryline106/data/scripts/isl.lua
+++ ImprovedStoryline106/data/scripts/isl.lua
@@ -1,8 +1,27 @@
 if ISL_VERSION == nil then
-	LOG("IMPROVED STORYLINE V1.0.6 BUILD 221104")
+	LOG("IMPROVED STORYLINE V1.0.6 BUILD 221112")
 
-	ISL_VERSION = "106-221104"
+	ISL_VERSION = "106-221112"
 
 	-- only for development needs, should not be in release!
 	EXECUTE_SCRIPT "data\\scripts\\compatch.lua"
+end
+
+function r1m1_FirstEnemyStart()
+	Fly("cam01", CINEMATIC_NO_AIM, 0, 5, 1, 1 )
+	StartCinematic()
+end
+
+function r1m1_FirstEnemySpawn()
+	CreateVehicleEx("Sml101","FirstB_0",CVector(2888.161, 346.333, 2342.007), 1002)
+
+	local FVehicle = GetEntityByName("FirstB_0")
+	if FVehicle then
+		FVehicle:SetRandomSkin()
+		FVehicle:SetRotation(Quaternion(0.021, 0.472, 0.040, 0.880))
+		FVehicle:SetThrottle( 1 )
+		FVehicle:SetCustomLinearVelocity( 25 )
+		FVehicle:SetExternalPathByName("fun1_5road")
+		FVehicle:setGodMode(1)
+	end
 end

747bc593ba83d022c9ed9d3ce735afcd27bf44cc
zatinu322 <pavl05@mail.ru>
2023-01-21 01:03:46 +0300

    Icons for PoI, scroll speed for narrator in r1m1, other bugfixes from dev 7

--- ImprovedStoryline106/data/scripts/isl.lua
+++ ImprovedStoryline106/data/scripts/isl.lua
@@ -4,6 +4,11 @@ if ISL_VERSION == nil then
 	ISL_VERSION = "106-HD-221230"
 end
 
+-- r1m1, показ текста рассказчика в начале игры
+function r1m1_ShowNarratorText()
+	AddCinematicMessage( 77771, 0.1)
+end
+
 -- r1m1, появление первого врага
 function r1m1_FirstEnemyStart()
 	local FVehicle = CreateVehicleEx("Sml101","FirstB_0",CVector(2888.161, 346.333, 2342.007), 1002)

69940bbb7f1c5c0ce4f1e70d6fe6a92ecb1b9d8e
zatinu322 <pavl05@mail.ru>
2023-03-12 20:35:36 +0300

    New build 230312 ready
    Vagon3 changing, removing Lisa car in r3m1 cutscene

--- ImprovedStoryline106/data/scripts/isl.lua
+++ ImprovedStoryline106/data/scripts/isl.lua
@@ -1,9 +1,9 @@
 -- Special settings for ISL (ComPatch version)
 
 if ISL_VERSION == nil then
-	LOG("IMPROVED STORYLINE V1.1 BUILD 230202")
+	LOG("IMPROVED STORYLINE V1.1 BUILD 230312")
 
-	ISL_VERSION = "110-230202"
+	ISL_VERSION = "110-230312"
 end
 
 -- локализация

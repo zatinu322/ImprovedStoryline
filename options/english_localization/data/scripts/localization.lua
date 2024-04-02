90dfbf7d426542484bcfeedca830efa5cc17e887
zatinu322 <68562524+zatinu322@users.noreply.github.com>
2024-04-03 01:46:51 +0300

    New file structure and eng loc initial implementation

---
+++ options/english_localization/data/scripts/localization.lua
@@ -0,0 +1,11 @@
+if LANG == nil then
+    LANG = "EN"
+    LOG("LOCALIZATION: "..LANG)
+end
+
+STR_PUNISH_BANDITS = "Attack bandits"
+STR_FIGHT_WITH_CLAW_R1M4 = "Boss Claw"
+STR_FIGHT_WITH_CLAW_R2M1 = "Boss Loader"
+STR_LABYRINTH = "Labyrinth"
+STR_BOSS_CHOOSE = "Before the Oracle"
+STR_BOSS_NJERI = "Njeri Battle"
